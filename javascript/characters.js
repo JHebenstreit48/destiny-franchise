339ba0ec5b06599125cd45636666744baa583c6a
Justin <j.hebenstreit25@gmail.com>
2024-07-02 19:24:05 -0500

    added more JavaScript code

--- javascript/characters.js
+++ javascript/characters.js
@@ -1,2 +1,37 @@
 console.log('Document Body: ');
 console.log(document.body);
+
+
+const yourGhost = document.querySelector('#your-ghost')
+const ghostDescrip = document.querySelector('#ghost-desc')
+
+const container = document.querySelector('.container');
+
+let isFlipped = false
+
+container.addEventListener('click', function (event) {
+    const element = event.target
+    
+
+
+    if (element.matches('.character-box')) {
+        if (isFlipped === false) {
+            // const state = element.getAttribute('data-state')
+            yourGhost.style.display = 'none'
+            ghostDescrip.style.display = 'block'
+        isFlipped === true
+
+        }
+
+        else {
+            yourGhost.style.display = 'block'
+            ghostDescrip.style.display = 'none'
+        isFlipped === false
+        }
+        
+
+        // if (state === 'hidden') {
+        //     element.textContent = element.dataset.
+        // }
+    }
+})
